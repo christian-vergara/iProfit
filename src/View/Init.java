161f88a91c3fec99346796e05841ad4fb498ec68
christian-vergara <Christian.Vergara531@gmail.com>
2017-09-25 01:14:07 -0400

    Item class created. First commit

--- src/View/Init.java
+++ src/View/Init.java
@@ -1,29 +1,51 @@
 package View;
 
+import com.sun.glass.ui.Menu;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.awt.*;
 
 /**
  * Created by Studio on 9/22/2017.
  */
 public class Init extends Application {
 
-    public static void Main(String args[]) {
+    private Stage primaryStage;
+    private Scene homeScene;
+
+    public static void main(String args[]) {
 
         launch(args);
 
     }
 
     public void start(Stage primaryStage) throws Exception {
+        this.primaryStage = primaryStage;
+
         primaryStage.setTitle("iProfit");
 
         BorderPane root = new BorderPane();
-        Scene homeScene = new Scene(root, 1280, 720);
+        homeScene = new Scene(root, 1280, 720);
 
+        root.getStylesheets().add("View/initstyle.css");
+
+
+        //SHOW STAGE
         primaryStage.setScene(homeScene);
         primaryStage.show();
-        primaryStage.close();
+
     }
+
+    public Stage getPrimaryStage() {
+        return primaryStage;
+    }
+
+    public void setPrimaryStage(Stage primaryStage) {
+        this.primaryStage = primaryStage;
+    }
+
+
 }
